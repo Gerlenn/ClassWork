808db5e81a77cd43316e3f3fed552048496fa468
Gerlenn <gleb_kozlov1998@mail.ru>
2022-10-02 21:36:11 +0300

    Created class Belorussian(Lesson 5)

---
+++ app/src/main/java/com/example/myclasswork/Belorussian.java
@@ -0,0 +1,26 @@
+package com.example.myclasswork;
+
+public class Belorussian extends Human{
+    public static void main(String[] args) {
+        Belorussian belorus = new Belorussian();
+        German german = new German();
+        belorus.speakLanguage();
+        german.speakLanguage();
+    }
+
+    @Override
+    void speakLanguage() {
+        System.out.println("Speak belorussian language");
+    }
+}
+
+class German extends Human{
+    @Override
+    void speakLanguage() {
+        System.out.println("Speak german language");
+    }
+}
+
+abstract class Human{
+    abstract void speakLanguage();
+}
