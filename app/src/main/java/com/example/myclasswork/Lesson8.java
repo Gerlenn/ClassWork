682aeed857ac0c52209dc77c8724cf43bcc9dd56
Gerlenn <gleb_kozlov1998@mail.ru>
2022-10-10 21:59:08 +0300

    Lesson 8

--- app/src/main/java/com/example/myclasswork/Lesson8.java
+++ app/src/main/java/com/example/myclasswork/Lesson8.java
@@ -1,8 +1,29 @@
 package com.example.myclasswork;
 
+import java.io.FileNotFoundException;
+
 public class Lesson8 {
 
     public static void main(String[] args) {
 
+        try {
+            throwUnCheckedException();
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            throwCheckedException();
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    static void throwUnCheckedException() throws NullPointerException {
+        throw new NullPointerException("runTime");
+    }
+
+    static void throwCheckedException() throws FileNotFoundException {
+        throw new FileNotFoundException("compileTime");
     }
 }
